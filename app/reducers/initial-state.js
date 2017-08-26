16c1b446e44a83e71c44e1b32d2071bc126ed8a1
Fabien BEHIER <fabien.behier@icloud.com>
2017-08-26 13:20:35 +0100

    add state

--- app/reducers/initial-state.js
+++ app/reducers/initial-state.js
@@ -1,5 +1,49 @@
+// types:
+// FIRE
+// WATER
+// WOOD
+// METAL
+// EARTH
+
 export default {
+  app: {
+    isReady: true,
+  },
   game: {
     isRunning: false,
+    roundDuration: 2000,
+    players:[{
+      master: { 
+        type: 'FIRE',
+        health: 10,
+        cursor: 0,
+      },
+      minions: [{
+        type: 'FIRE',
+        health: 1,
+      }, {
+        type: 'WATER',
+        health: 2,
+      }, {
+        type: 'METAL',
+        health: 3,
+      }]
+    }, {
+      master: { 
+        type: 'WATER',
+        health: 10,
+        cursor: 1,
+      },
+      minions: [{
+        type: 'WATER',
+        health: 1,
+      }, {
+        type: 'METAL',
+        health: 3,
+      }, {
+        type: 'FIRE',
+        health: 2,
+      }]
+    }]
   },
 }
