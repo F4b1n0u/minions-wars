ac63cb67b032b557cd5eb2476bec699c4de785ec
Daniel Jimenez <d.jimenezcabrera@reply.com>
2017-08-26 17:28:45 +0100

    Add minions

--- app/components/player.js
+++ app/components/player.js
@@ -11,19 +11,45 @@ export default class PlayerComponent extends React.Component {
     } = this.props
 
     return (
-      <Master
-       {...master}
-       {...rest}
-      />
+      <Wrapper>
+        <Master
+          {...master}
+          {...rest}
+        />
+
+        <Minion left={10} />
+        <Minion left={40} />
+        <Minion left={70} />
+        <Minion left={100} />
+      </Wrapper>
     )
   }
 }
+
+const Wrapper = styled.View`
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+`
+
+const Minion = CreatureComponent.extend`
+  position:absolute;
+  height: 30;
+  width: 23;
+  bottom: 29;
+  ${props => (props.left) ? `left: ${props.left};` : ''}
+  ${props => (props.right) ? `right: ${props.right};` : ''}
+`
 
 const Master = CreatureComponent.extend`
   position:absolute;
   height: 115;
   width: 87;
-  top: 175;
+  bottom: 29;
   ${props => (props.left) ? `left: ${props.left};` : ''}
   ${props => (props.right) ? `right: ${props.right};` : ''}
 `
