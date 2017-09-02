6906ba703ffee7b4005b5582fcba14a7e33792db
Daniel Jimenez <d.jimenezcabrera@reply.com>
2017-09-02 13:14:10 +0100

    Use constant types to define creature types

--- app/components/creature.js
+++ app/components/creature.js
@@ -1,27 +1,35 @@
 import React from 'react'
 import styled from 'styled-components/native'
 
+import {
+  EARTH,
+  FIRE,
+  METAL,
+  WATER,
+  WOOD,
+} from '@utils/types'
+
 const backgroundImage = require('../../assets/images/creatures/green/playerGreen_up1.png')
 
 const creaturesType = {
-  FIRE: {
+  [FIRE]: {
     backgroundImage: require('../../assets/images/creatures/red/playerRed_up1.png'),
   },
-  WATER: {
+  [WATER]: {
     backgroundImage: require('../../assets/images/creatures/blue/playerBlue_up1.png'),
   },
-  WOOD: {
+  [WOOD]: {
     backgroundImage: require('../../assets/images/creatures/green/playerGreen_up1.png'),
   },
-  METAL: {
+  [METAL]: {
     backgroundImage: require('../../assets/images/creatures/grey/playerGrey_up1.png'),
   },
-  EARTH: {
+  [EARTH]: {
     backgroundImage: require('../../assets/images/creatures/green/playerGreen_up1.png'),
   }
 }
 
 export default styled.Image.attrs({
-    resizeMode: 'cover',
-    source: props => creaturesType[props.type].backgroundImage,
-  })``
+  resizeMode: 'cover',
+  source: props => creaturesType[props.type].backgroundImage,
+})``
